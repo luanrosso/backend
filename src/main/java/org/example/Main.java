5b8adc3e8d663aea4fa0c18771f1cc938765e880
luanrosso <rosso.luan@gmail.com>
2024-08-20 20:33:27 -0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("Qual ambientação você prefere?");
+        System.out.println("1 - Sci fi");
+        System.out.println("2 - Medieval");
+        Integer opcaoAmbientacao = leitor.nextInt();
+
+
+        System.out.println("Qual gênero você prefere?");
+        System.out.println("1 - Comédia");
+        System.out.println("2 - Drama");
+        Integer opcaoGenero = leitor.nextInt();
+
+
+        if (opcaoAmbientacao == 1 && opcaoGenero == 1) System.out.println("Homens De Preto");
+        if (opcaoAmbientacao == 1 && opcaoGenero == 2) System.out.println("Arrival");
+        if (opcaoAmbientacao == 2 && opcaoGenero == 1) System.out.println("Shrek");
+        if (opcaoAmbientacao == 2 && opcaoGenero == 2) System.out.println("Gladiador");
+    }
+}
